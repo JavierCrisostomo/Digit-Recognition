9cea16696fa57998a1e62fd319f6fdae2aba3ddf
Brian Walker <bdwalker@cs.washington.edu>
2013-05-18 15:04:16 -0700

    Added code for main to process data and run a basic svm test

--- Digit Recognition/Main.py
+++ Digit Recognition/Main.py
@@ -1,39 +1,45 @@
 import numpy as np
+import SVM
+import Test
+import Train
 import sys
+import Kernels
 
 output = None
 train_input = None
 test_input = None
-y = None
 train_data = None
 test_data = None
 
 
 def loadTrainData():
     global train_data, train_input, y
-    train_data = np.loadtxt(open(train_input, "r"), delimiter=",", skiprows=1)
-    y = train_data[:, 0]
-
-    # delete label
-    train_data = np.delete(train_data, 0, 1)
+    train_data = np.loadtxt(train_input, delimiter=",", skiprows=1)
 
 
 def loadTestData():
     global test_data, test_input
-    test_data = np.loadtxt(open(test_input, "r"), delimiter=",", skiprows=1)
+    test_data = np.loadtxt(test_input, delimiter=",", skiprows=1)
 
 
 def main():
     if len(sys.argv) < 4:
         print "usage: train_file test_file output_directory"
 
-    global train_input, test_input, output
+    global train_input, test_input, output, y, test_data
     train_input = sys.argv[1]
     test_input = sys.argv[2]
     output = sys.argv[3]
 
     loadTrainData()
-    loadTestData()
+    #loadTestData()
+
+    trainer = Train.Trainer()
+    #trainer.processData(train_data, "./model/parsed_train_data")
+    #trainer.loadProcessedData("./model/parsed_train_data")
+    #trainer.trainModel()
+    svm = SVM.SVM(train_data, 0, kernel=Kernels.exactPolyKernel, dim=5, report=True)
+    svm.train_svm()
 
 if __name__ == "__main__":
     main()
